bdb62b904f1bbbd043137cd89bef03ec6f3e9f8d
sasha <sasha06122002@gmail.com>
2022-04-14 23:39:35 +0300

    tests rewrite, fixes in Du class

--- src/main/kotlin/Du.kt
+++ src/main/kotlin/Du.kt
@@ -1,12 +1,10 @@
 import org.apache.commons.io.FileUtils.sizeOfDirectory
 import java.io.*
-import java.math.BigDecimal
-import kotlin.collections.ArrayList
 
 class Du(private val humanForm: Boolean,
          private val totalSize: Boolean,
          private val base: Boolean,
-         ) {         //private val filesNames: List<String>
+         ) {
 
     fun getSizeKB(fileStr: String): Double {
         val isBaseSI = isSI(base)
@@ -20,97 +18,30 @@ class Du(private val humanForm: Boolean,
         return sizeKB
     }
 
+    fun size(sizeKB: Double): String {
+        val isBaseSI = isSI(base)
+        return if (humanForm) {
+            var unit = arrayListOf(" KB", " MB", " GB")
+            var sizeKB0 = (sizeKB * 10).toInt() / 10.0
+            var k = 0
+            if (base) unit = arrayListOf(" KiB", " MiB", " GiB")
+            while (sizeKB0 > isSI(base) - 1) {
+                sizeKB0 = (sizeKB0 / isBaseSI)
+                k += 1
+            }
+            "" + sizeKB0 +  unit[k]
+        } else {
+            "" + (sizeKB / isBaseSI)
+        }
+    }
+
     fun isSI(base: Boolean): Double {
         var isBaseSI = 1024.0
         if (base) isBaseSI = 1000.0
         return isBaseSI
     }
 
-
-//    fun units(base: Boolean): List<String> {
-//        var isUnitsSI = arrayListOf<String>(" KB", " MB", " GB")
-//        if (base) isUnitsSI = arrayListOf(" KiB", " MiB", " GiB")
-//        return isUnitsSI
-//    }
-
-
-//    fun getSizesInHumanForm(filesNames: List<String>, base: Boolean): List<String> {
-//        val isUnitsSI = units(base)
-//        val isBaseSI = isSI(base)
-//        val sizesInHumanFormat = ArrayList<String>()
-//        for (i in 0..filesNames.size - 1) {
-//            var sizeInHumanFormat = getSizeKB(filesNames[i], base)
-//            if (sizeInHumanFormat < isBaseSI) {
-//                val size = "$sizeInHumanFormat" + isUnitsSI[0]
-//                sizesInHumanFormat.add(size)
-//            }
-//            if (sizeInHumanFormat >= isBaseSI && sizeInHumanFormat < isBaseSI * isBaseSI) {
-//                sizeInHumanFormat = ((sizeInHumanFormat / isBaseSI) * 100).toInt() / 100.0
-//                val size = "$sizeInHumanFormat" + isUnitsSI[1]
-//                sizesInHumanFormat.add(size)
-//            }
-//            if (sizeInHumanFormat >= isBaseSI * isBaseSI) {
-//                sizeInHumanFormat = ((sizeInHumanFormat / (isBaseSI * isBaseSI)) * 100).toInt() / 100.0
-//                val size = "$sizeInHumanFormat" + isUnitsSI[2]
-//                sizesInHumanFormat.add(size)
-//            }
-//        }
-//        return sizesInHumanFormat
-//    }
-
-//    fun getSizesKB(filesNames: List<String>, base: Boolean): List<String> {
-//        val sizes = ArrayList<String>()
-//        for (i in 0..filesNames.size - 1) {
-//            val size = getSizeKB(filesNames[i], base)
-//            sizes.add("$size")
-//        }
-//        return sizes
-//    }
-
-//    fun getTotalSizeKB(filesNames: List<String>, base: Boolean): Double {
-//        val sizes = getSizesKB(filesNames, base)
-//        var totalSizeInKB = 0.0
-//        for (i in 0..sizes.size - 1) {
-//            totalSizeInKB += sizes[i].toDouble()
-//        }
-//        return (totalSizeInKB * 10).toInt() / 10.0
-//    }
-
-//    fun getTotalInHumanForm(filesNames: List<String>, base: Boolean): String {
-//        val isUnitsSI = units(base)
-//        val isBaseSI = isSI(base)
-//        var totalSizeInHumanForm = getTotalSizeKB(filesNames, base)
-//        var unit = isUnitsSI[0]
-//        if (totalSizeInHumanForm >= isBaseSI && totalSizeInHumanForm < isBaseSI * isBaseSI) {
-//            totalSizeInHumanForm = ((totalSizeInHumanForm / isBaseSI) * 100).toInt() / 100.0
-//            unit = isUnitsSI[1]
-//        }
-//        if (totalSizeInHumanForm >= isBaseSI * isBaseSI) {
-//            totalSizeInHumanForm = ((totalSizeInHumanForm / (isBaseSI * isBaseSI)) * 100).toInt() / 100.0
-//            unit = isUnitsSI[2]
-//        }
-//        return "$totalSizeInHumanForm" + unit
-//    }
-
-    fun du(filesNames: List<String>): String {
-//    val result = StringBuilder()
-//    if ((humanForm && base) || (humanForm)) {
-//        result.append(getSizesInHumanForm(filesNames, base))
-//        val sizes = getSizesInHumanForm(filesNames, base)
-//        for (i in 0..sizes.size - 1) println("File number $i: " + sizes[i])
-//    } else {
-//        result.append(getSizesKB(filesNames, base))
-//        val sizes = getSizesKB(filesNames, base)
-//        for (i in 0..sizes.size - 1) println("File number $i: " + sizes[i])
-//    }
-//
-//    if ((totalSize && humanForm && base) || (totalSize && humanForm)) {
-//        result.append(getTotalInHumanForm(filesNames, base))
-//        println("Total: " + getTotalInHumanForm(filesNames, base))
-//    } else if (totalSize) {
-//        result.append(getTotalSizeKB(filesNames, base))
-//        println("Total: " + getTotalSizeKB(filesNames, base))
-//    }
+    fun du(filesNames: List<String>) {
 
         for (i in filesNames.indices) {
             println(size(getSizeKB(filesNames[i])))
@@ -123,30 +54,5 @@ class Du(private val humanForm: Boolean,
             }
             print((size(total)))
         }
-        return ""
-    }
-
-//    fun main(args: Array<String>) {
-//        DuParser.main(args)
-//    }
-
-
-    fun size(sizeKB: Double): String {
-        return if (humanForm) {
-            var unit = arrayListOf(" KB", " MB", " GB")
-            var sizeKB0 = sizeKB
-            var k = 0
-            if (base) unit = arrayListOf(" KiB", " MiB", " GiB")
-            while (sizeKB0 > isSI(base) - 1) {
-                sizeKB0 /= isSI(base)
-                k += 1
-            }
-            "" + sizeKB0 +  unit[k]
-        } else {
-            "" + sizeKB / isSI(base)
-        }
     }
 }
-
-
-
--- src/test/kotlin/DuTest.kt
+++ src/test/kotlin/DuTest.kt
@@ -1,63 +1,37 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-
+import java.io.File
+import org.apache.commons.io.FileUtils.sizeOfDirectory
 
 class DuTest {
     @Test
-    fun getSizeKBTest() {
-        assertEquals(getSizeKB("src\\files\\file1", true), 14.8)
-        assertEquals(getSizeKB("src\\files\\file1", false), 14.4)
-        assertEquals(getSizeKB("src\\files\\directory1", false), 778.9)
-        assertEquals(getSizeKB("src\\files\\directory1", true), 797.6)
+    fun getSizeTest1() {
+        val dir = File("src\\files\\directory1")
+        val file1 = File("src\\files\\file1")
+        val file2 = File("src\\files\\file2")
+        val du = Du(false, false, false)
+
+        val size1 = du.getSizeKB("src\\files\\directory1")
+        assertEquals(size1 , (sizeOfDirectory(dir) / 1024.0 * 10).toInt() / 10.0)
+        val size2 = du.getSizeKB("src\\files\\file1")
+        assertEquals(size2 , (file1.length() / 1024.0 * 10).toInt() / 10.0)
+        val size3 = du.getSizeKB("src\\files\\file2")
+        assertEquals(size3 , (file2.length() / 1024.0 * 10).toInt() / 10.0)
     }
+
 
     @Test
-    fun isSITest() {
-        assertEquals(isSI(true), 1000.0)
-        assertEquals(isSI(false), 1024.0)
+    fun getSizeTest2() {
+        val dir = File("src\\files\\directory1")
+        val file1 = File("src\\files\\file1")
+        val file2 = File("src\\files\\file2")
+        val du = Du(false, false, true)
+
+        val size1 = du.getSizeKB("src\\files\\directory1")
+        assertEquals(size1 , (sizeOfDirectory(dir) / 1000.0 * 10).toInt() / 10.0)
+        val size2 = du.getSizeKB("src\\files\\file1")
+        assertEquals(size2 , (file1.length() / 1000.0 * 10).toInt() / 10.0)
+        val size3 = du.getSizeKB("src\\files\\file2")
+        assertEquals(size3 , (file2.length() / 1000.0 * 10).toInt() / 10.0)
     }
-
-    @Test
-    fun unitsTest() {
-        assertEquals(units(true), listOf(" KiB", " MiB", " GiB"))
-        assertEquals(units(false), listOf(" KB", " MB", " GB"))
-    }
-
-    @Test
-    fun getSizeInHFTest() {
-        assertEquals(getSizesInHumanForm(listOf("src\\files\\file1",
-            "src\\files\\file2"), false), listOf("14.4 KB", "81.4 KB"))
-        assertEquals(getSizesInHumanForm(listOf("src\\files\\file1",
-            "src\\files\\file2"), true), listOf("14.8 KiB", "83.4 KiB"))
-        assertEquals(getSizesInHumanForm(listOf("src\\files\\file1", "src\\files\\file2",
-            "src\\files\\directory1"), false), listOf("14.4 KB", "81.4 KB", "778.9 KB"))
-        assertEquals(getSizesInHumanForm(listOf("src\\files\\file1", "src\\files\\file2",
-            "src\\files\\directory1"), true), listOf("14.8 KiB", "83.4 KiB", "797.6 KiB"))
-    }
-
-    @Test
-    fun getSizesKBTest() {
-        assertEquals(getSizesKB(listOf("src\\files\\file1", "src\\files\\file2",
-            "src\\files\\directory1"), false), listOf("14.4", "81.4", "778.9"))
-        assertEquals(getSizesKB(listOf("src\\files\\file1", "src\\files\\file2",
-            "src\\files\\directory1"), true), listOf("14.8", "83.4", "797.6"))
-    }
-
-    @Test
-    fun getTotalSizeKBTest() {
-        assertEquals(getTotalSizeKB(listOf("src\\files\\file1", "src\\files\\file2",
-            "src\\files\\directory1"), true), 895.8)
-        assertEquals(getTotalSizeKB(listOf("src\\files\\file1", "src\\files\\file2",
-            "src\\files\\directory1"), false), 874.7)
-    }
-
-    @Test
-    fun getTotalInHFTest() {
-        assertEquals(getTotalInHumanForm(listOf("src\\files\\file1", "src\\files\\file2",
-            "src\\files\\directory1"), false), "874.7 KB")
-        assertEquals(getTotalInHumanForm(listOf("src\\files\\file1", "src\\files\\file2",
-            "src\\files\\directory1"), true), "895.8 KiB")
-    }
-
-
 }
